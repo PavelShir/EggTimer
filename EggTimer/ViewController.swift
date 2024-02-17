68841c28a01f3b29425f7912da8a232111de3f6b
Pavel Shirokiy <pshirokiy@gmail.com>
2024-02-17 23:50:39 +0300

    Create new app

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,54 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet var doneLabel: UILabel!
+    @IBOutlet var progressBarInfo: UIProgressView!
+    
+    var secondsRemaining = 60
+    var timer = Timer()
+    var player: AVAudioPlayer!
+    
+    let eggTime = [
+        "Soft" : 3,
+        "Medium" : 4,
+        "Hard" : 7
+    ]
+    
+    override func viewDidLoad() {
+        doneLabel.text = "Выбери, как сварить яйца"
+        progressBarInfo.setProgress(0, animated: true)
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        timer.invalidate()
+        progressBarInfo.setProgress(0, animated: true)
+        
+        guard let buttonTitile = sender.currentTitle else { return }
+        secondsRemaining = eggTime[buttonTitile]!
+        
+        doneLabel.text = buttonTitile
 
-
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds")
+            secondsRemaining -= 1
+            progressBarInfo.setProgress(Float(secondsRemaining), animated: true)
+        } else {
+            doneLabel.text = "Done"
+            
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+            
+            timer.invalidate()
+        }
+    }
 }
